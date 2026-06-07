d900f0dfd6138f4ebb443c2903aa42b20bc4fb8e
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-06-07 19:14:09 +0200

    Add class Linear for Linear layers

--- src/models/v4/nn.py
+++ src/models/v4/nn.py
@@ -2,3 +2,23 @@
 # - Linear
 # - Tanh
 # - BatchNorm1d
+
+import torch
+import torch.nn.functional as F
+
+class Linear:
+    def __init__(self, fan_in: int, fan_out: int, has_bias = True):
+        self.w = torch.randn(fan_in, fan_out) / torch.tensor(fan_in, dtype=torch.float)**0.5 # Kaiming initialization
+        if has_bias:
+            self.b = torch.zeros(fan_out)
+        else:
+            self.b = None
+    
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        y = x @ self.w
+        if self.b is not None:
+            y += self.b
+        return y
+    
+    def paremeters(self) -> list[torch.Tensor]:
+        return [self.w] + ([] if self.b is None else [self.b])
