31a49fccd12c133043ab6bf00874231c40c54c2c
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-06-24 13:03:42 +0200

    Add BatchNorm1d class

--- src/models/v4/nn.py
+++ src/models/v4/nn.py
@@ -29,3 +29,45 @@ class Tanh:
         return torch.tanh(x)
     def parameters(self) -> list[torch.Tensor]:
         return []
+
+class BatchNorm1d:
+    '''
+    Normalizes the inputs' standard deviation.
+    
+    Batch normalization layer based on the Ioffe et al., 2015 paper.
+    '''
+    def __init__(self, features: int, eps = 1e-5, momentum = 0.05):
+        
+        self.gamma = torch.randn(features)
+        self.beta  = torch.zeros(features)
+        
+        self.eps = eps
+        self.momentum = momentum
+        self.training = True
+        
+        self.running_mean = torch.ones(features)
+        self.running_var  = torch.ones(features)
+        
+    def __call__(self, x: torch.Tensor):
+        
+        # shape of x: (batch_size, features), if training,
+        #             (features) else
+        
+        if self.training:
+            mean = x.mean(dim=0)
+            var = x.var(dim=0)
+            
+            self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * mean
+            self.running_var = (1 - self.momentum) * self.running_var + self.momentum * var
+        else:
+            mean = self.running_mean
+            var = self.running_var
+        
+        normalized = (x - mean) / torch.sqrt(var + self.eps)
+        
+        y = self.gamma * normalized + self.beta # scale and shift
+        
+        return y
+    
+    def parameters(self) -> list[torch.Tensor]:
+        return [self.gamma, self.beta]
